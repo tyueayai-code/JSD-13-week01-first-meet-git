339dc727870114aeae3633a168539a3152c5b214
unknown <tyueayai@gmail.com>
2026-07-02 13:38:06 +0700

    added data in an array and object,rendered in html page

---
+++ my-object.js
@@ -0,0 +1,44 @@
+let foodDatabase = [
+  {
+    id:1,
+    name: "Pizza",
+    cuisine:"Italian",
+    origin: "Italy",
+    vegetarian: false,
+  },
+  {
+    id:2,
+    name: "Malatang",
+    cuisine:"Chinese",
+    origin: "China",
+    vegetarian: false,
+  },
+  {
+    id:3,
+    name: "Pad Thai",
+    cuisine:"Thai",
+    origin: "Thailand",
+    vegetarian: false,
+  },
+  {
+    id:4,
+    name: "Burger",
+    cuisine:"American",
+    origin: "United States",
+    vegetarian: false,
+  },
+  {
+    id:5,
+    name: "Sushi",
+    cuisine:"Japanese",
+    origin: "Japan",
+    vegetarian: false,
+  },
+  {
+    id:6,
+    name: "Medovik",
+    cuisine:"Russian",
+    origin: "Russia",
+    vegetarian: false,
+  }
+]
